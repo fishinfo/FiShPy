aa9691099d9112fb6539bf1e688cfe07ad4fa8c7
Fabian Guignard <35561968+fguignard@users.noreply.github.com>
2019-08-23 16:02:07 +0200

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,22 @@
+=====================================
+FiShPy
+=====================================
+
+.. image:: https://pypip.in/d/FiShPy/badge.png
+        :target: https://pypi.python.org/pypi/FiShPy
+
+
+Python package for Fisher-Shannon method
+On-going work (uncomplete work)
+
+* Free software: MIT license
+* Documentation: https://fishinfo.github.io
+
+Require
+--------
+
+* numpy: 1.15.4
+* scipy: 1.3.4
+
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'FiShPy',         
+  packages = ['FiShPy'],   
+  version = '1.0',      
+  license='MIT',        
+  description = 'Fisher-Shannon method (uncomplete work)', 
+  author = 'Fabian Guignard',               
+  author_email = 'fabian.guignard@bluemail.ch',    
+  url = 'https://github.com/fishinfo/FiShPy', 
+  download_url = 'https://github.com/fishinfo/FiShPy/archive/v_10.tar.gz',   
+  keywords = [
+      'Fisher Shannon plane', 
+      'Fisher information measure', 
+      'Fisher-Shannon complexity', 
+      'Statistical complexity',
+      ],  
+  install_requires=[     
+          'numpy',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Topic :: Software Development :: Build Tools',    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.7',
+    ],
+)
